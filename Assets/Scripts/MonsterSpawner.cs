3376a204463692371e07b515fa419bb3ebf4090e
ZhijianWangJohn <52527069+zljtt@users.noreply.github.com>
2024-01-17 18:19:22 -0500

    Update MonsterSpawner.cs

--- Assets/Scripts/MonsterSpawner.cs
+++ Assets/Scripts/MonsterSpawner.cs
@@ -29,8 +29,6 @@ public class MonsterSpawner : MonoBehaviour
         GenerateNextMonster();
         GenerateNextMonster();
         GenerateNextMonster();
-        GenerateNextMonster();
-        GenerateNextMonster();
         UpdateTargetColor();
     }
 
